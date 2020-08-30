d278beeceee4608a5e220f5381e49922e1bf8b1b
RabbitOTM <RyanTommyTavares@gmail.com>
2020-08-29 20:54:06 -0700

    Updates

---
+++ precache-manifest.b0befaf79b54dbc8c26bfdf464e998d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9005ded3ee8497349b3e478f35b740c",
+    "url": "/video-game-memorial/index.html"
+  },
+  {
+    "revision": "66aa6cdb66d4e49af9ad",
+    "url": "/video-game-memorial/static/js/2.658097ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-game-memorial/static/js/2.658097ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d43faff42f29fd1285b",
+    "url": "/video-game-memorial/static/js/main.41033fbf.chunk.js"
+  },
+  {
+    "revision": "0d9abc0861ed84e3d904",
+    "url": "/video-game-memorial/static/js/runtime-main.fdddf57e.js"
+  }
+]);
